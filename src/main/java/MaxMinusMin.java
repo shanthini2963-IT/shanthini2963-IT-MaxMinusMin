e17eb9e9d18f2b0c0b3082148cd8f625278fa9c0
shanthini2963-IT <shanthiniramanjaneya@gmail.com>
2025-04-17 20:43:56 +0530

    coding challenge 5

--- src/main/java/MaxMinusMin.java
+++ src/main/java/MaxMinusMin.java
@@ -10,6 +10,17 @@ public class MaxMinusMin {
      * @return the max value of nums minus the min value of nums.
      */
     public int difference(List<Integer> nums){
-        return 0;
+        int min = nums.get(0);
+        int max = nums.get(0);
+
+        for (int num : nums) {
+            if (num < min) {
+                min = num;
+            }
+            if (num > max) {
+                max = num;
+            }
+        }
+        return max - min;
     }
 }
